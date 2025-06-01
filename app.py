2b898fb9e2c2b705b30880229430dccd4c330325
P-cy <poeurmessi67@gmail.com>
2025-06-01 23:11:42 +0700

    fix path

--- app.py
+++ app.py
@@ -264,7 +264,7 @@ def load_model():
     try:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         model = vit_base_patch32_model(num_classes=5)
-        model_path = "assets/model/best_model.pth"
+        model_path = "streamlit/assets/model/best_model.pth"
         
         if torch.cuda.is_available():
             checkpoint = torch.load(model_path)
